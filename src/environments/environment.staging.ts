36df17a996e394e417aeb1938a2dfeb3eddfda29
Mawele <mbizi.kevin@gmail.com>
2025-11-03 12:20:15 +0100

    upadate the features

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true, // staging sera en ligne, donc app buildée aussi !
+  name: 'staging',
+  apiUrl: '<url-of-your-staging-api>' // Ex : staging-api.fromphacotohero.fr
+};
