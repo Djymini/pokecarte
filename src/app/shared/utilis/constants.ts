92eb32db9456ea4b0c2defe9dfee695e306139d1
Mawele <mbizi.kevin@gmail.com>
2025-11-08 14:09:36 +0100

    the base of header and installation of tailwind is make

--- src/app/shared/utilis/constants.ts
+++ src/app/shared/utilis/constants.ts
@@ -1,1 +1,9 @@
-export const NAME_APP: string = "Pokécarte";
+export const APP_NAME: string = "Pokécarte";
+
+export const HOME_TITLE: string = "Accueil";
+export const PRODUCTS_TITLE: string = "En vente";
+export const ABOUT_TITLE: string = "A propos";
+export const SUPPORT_TITLE: string = "Support";
+export const LOGIN_TITLE: string = "Connexion";
+export const LOGOUT_TITLE: string = "Deconnexion";
+
