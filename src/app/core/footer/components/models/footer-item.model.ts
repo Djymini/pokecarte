c88d25dca7dd324be2c7b6d441e5c97f8f721156
Mawele <mbizi.kevin@gmail.com>
2025-11-08 17:01:23 +0100

    make the new footer

---
+++ src/app/core/footer/components/models/footer-item.model.ts
@@ -0,0 +1,9 @@
+export type FooterItem = {
+  title:string;
+  links:FooterLink[];
+}
+
+export type FooterLink = {
+  name:string;
+  url:string;
+}
--- src/app/shared/utilis/constants.ts
+++ src/app/shared/utilis/constants.ts
@@ -6,4 +6,14 @@ export const ABOUT_TITLE: string = "A propos";
 export const SUPPORT_TITLE: string = "Support";
 export const LOGIN_TITLE: string = "Connexion";
 export const LOGOUT_TITLE: string = "Deconnexion";
+export const INFO_TITLE: string =  "Informations légales";
+export const SALE_INFO_TITLE: string = "Conditions générales de vente";
+export const CGU_TITLE: string = "Conditions générales d’utilisation";
+export const LEGAL_NOTICE_TITLE: string = "Mentions légales";
+export const COOKIE_INFO_TITLE: string = "Protection de la vie privée et des cookies";
+export const HELP_TITLE: string = "Aide et support";
+export const USER_TITLE: string = "Espace client";
+export const USER_DASHBOARD_TITLE: string = "Accueil de l’espace client";
+export const ORDER_TITLE: string = "Mon suivi de commande";
+
 
