9ea180f4ff20b88b28e45348c49fdbb1a97adc55
Mawele <mbizi.kevin@gmail.com>
2025-12-05 12:19:20 +0100

    Fix environments

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true, // staging sera en ligne, donc app buildée aussi !
   name: 'staging',
-  apiUrl: 'staging.pokecarte-api.loutredev.fr' // Ex : staging-api.fromphacotohero.fr
+  apiUrl: 'https://staging.pokecarte-api.loutredev.fr', // Ex : staging-api.fromphacotohero.fr
 };
